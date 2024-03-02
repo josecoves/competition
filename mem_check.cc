ef2b60b2f6950bc5dcd9722e2212aa17425006a9
VCoves <32782704+VCoves@users.noreply.github.com>
2024-03-02 22:22:47 +0100

    Create mem_check.cc

---
+++ mem_check.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline void JJ();
+
+
+
+/* DATA STRUCTURES */
+bool ST; //FOR MEM CHECKING
+int n,k;
+const int N = 1'000'000;
+int a[N];
+/* ********************** */
+
+/* INPUT */
+void _input() {
+	cin >> n;
+}
+
+int rando(int a, int b) {
+	return a + rand() % (b - a + 1);
+}
+
+void gen_input() {
+	n = rando(4,10);
+}
+/*********************** */
+
+/* SOLVE */
+void _solve() {
+	return;
+}
+
+
+
+int main(){
+	JJ();
+}
+bool ED;
+
+inline void JJ(){
+	double MIB = fabs((&ED-&ST)/1048576.), LIM = 125;
+  	cerr << "MEMORY: " << MIB << " MB"<< endl, assert(MIB<=LIM);
+}
