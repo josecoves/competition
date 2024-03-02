fa77d382e06897d5efa0f2ab9b05ceb9a5ef2ee8
VCoves <32782704+VCoves@users.noreply.github.com>
2024-03-02 22:12:39 +0100

    Update template_base.cc

--- template_base.cc
+++ template_base.cc
@@ -1,3 +1,13 @@
+template <class T>
+using vc = vector<T>;
+template <class T>
+using vvc = vector<vc<T>>;
+template <class T>
+using vvvc = vector<vvc<T>>;
+template <class T>
+using vvvvc = vector<vvvc<T>>;
+template <class T>
+using vvvvvc = vector<vvvvc<T>>;
 
 #define vvv(type, name, h, w, ...)   \
   vector<vector<vector<type>>> name( \
